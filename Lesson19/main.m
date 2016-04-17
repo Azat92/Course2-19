96c9bf69b7a19c5e596854fe91fcde5b2893db76
Azat Almeev <azat.almeev@gmail.com>
2016-04-17 17:21:41 +0300

    Initial Commit

---
+++ Lesson19/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lesson19
+//
+//  Created by Azat Almeev on 17.04.16.
+//  Copyright © 2016 Azat Almeev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
